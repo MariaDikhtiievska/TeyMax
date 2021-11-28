e24e23ee1b520b951daa1349b4b504d3830ed75f
Maria <kn201_dms@student.ztu.edu.ua>
2021-11-29 00:37:08 +0200

    Task 6 was modified as addirional task

--- Lesson06-version2/index.js
+++ Lesson06-version2/index.js
@@ -25,51 +25,78 @@ let arr = [                 //array of objects
         relations: ['pm']
     }
 ]
-let htmlElements = []   // array of dives
-function create(){
+let htmlElements = new Map() // Map of HTMLDivElements
+//let htmlElements = []   // array of dives
+const createWrapper = () =>{
     let wrapper = document.createElement("div")
     wrapper.classList.add("wrapper")
     wrapper.setAttribute("id", "wrapper")
+    return wrapper
+}
+const create = () => {
+    const wrapper = createWrapper();
     document.getElementsByTagName('body')[0].appendChild(wrapper)
 
-    arr.forEach(function (item, index, arr){
-        htmlElements[index] = document.createElement("div")
-        htmlElements[index].textContent = item.content
-        htmlElements[index].setAttribute("id",item.id)
 
-        htmlElements[index].classList.add("initial")
-        document.getElementById("wrapper").insertAdjacentElement("afterbegin", htmlElements[index])
+    arr.forEach(function (item, index, arr){
+
+htmlElements.set(item.id,document.createElement("div"))
+        htmlElements.get(item.id).textContent = item.content
+        htmlElements.get(item.id).setAttribute("id",item.id)
+        htmlElements.get(item.id).classList.add("initial")
+
+
+
+        document.getElementById("wrapper").insertAdjacentElement("afterbegin", htmlElements.get(item.id))
         //appendChild(htmlElements[index])
 
-
-        clicked(htmlElements[index]) // div element
+        subscribeClickEvent(htmlElements.get(item.id)) // div element
 
 
     })
 
 }
-function clicked(div){
+const subscribeClickEvent = (div) => {
 
     div.addEventListener('click', () => {
-        if (div.classList.value.split(' ')[1] === "clickedElem"){
+        const {id: clickedElementId } = div;
 
-            for (let i of htmlElements) {
-                i.classList.remove('clickedRelations')
-                i.classList.remove('clickedElem')
-            }
+        const element = htmlElements.get(clickedElementId)
+
+        const [_, checkClass] = element.classList
+
+        if (checkClass === "clickedElem"){
+     //  if (div.classList.value.split(' ')[1] === "clickedElem"){
+            htmlElements.forEach((item) =>{
+                item.classList.remove('clickedRelations')
+                item.classList.remove('clickedElem')
+            })
+            //for (let i of htmlElements) {
+            //    i.classList.remove('clickedRelations')
+              //  i.classList.remove('clickedElem')
+            //}
         }
         //div.classList.toggle("clickedElem")
         else {
-            arr.forEach(function (item, index, arr) {
-                htmlElements[index].classList.remove('clickedRelations')
-                htmlElements[index].classList.remove('clickedElem')
+            //arr.forEach(function (item, index, arr) {
+              //  htmlElements[index].classList.remove('clickedRelations')
+               // htmlElements[index].classList.remove('clickedElem')
+            let keys = [...htmlElements.keys()]
 
-                if (item.relations.includes(div.id)) {
-                    htmlElements[index].classList.add("clickedRelations")
+            arr.forEach(function (item, index, arr) {
+                htmlElements.get(item.id).classList.remove('clickedRelations')
+                htmlElements.get(item.id).classList.remove('clickedElem')
+                console.log(item)
+                console.log(htmlElements.get(item.id))
+                console.log(keys[index])
+                if (item.relations.includes(clickedElementId)) {
+                    htmlElements.get(item.id).classList.add("clickedRelations")
                 }
 
             })
-            div.classList.add("clickedElem")
+
+            element.classList.add("clickedElem")
+
         }
     })
 }
