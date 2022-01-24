c2563f6b0468180353fa46ef0ec7b44f952c8af9
Maria <kn201_dms@student.ztu.edu.ua>
2022-01-25 00:04:51 +0200

    LikesCounter has been modified

--- React1/les/src/elements/LikesCounter.jsx
+++ React1/les/src/elements/LikesCounter.jsx
@@ -4,23 +4,27 @@ import styles from '../components/styles.module.scss'
 
 const LikesCounter = ({ id, likes, update }) => {
    const [liked,setlike] = useState(false)
+   
     const addLike = () => {
         setlike(!liked)
-        update((arr)=>arr.map(item=>{
-            if(item.id===id){
-                if(liked){
-                    item.likes -= 1
-            
-                }else{
-                    item.likes += 1
-                 }
+        update((arr)=> arr.map(item => {
+            if (item.id === id) {
+                const likes = liked ? item.likes - 1 : item.likes + 1
+
+                return {
+                    ...item,
+                    likes: likes
+                }
             }
             return item
         }))
 
     };
+    const style = {
+        color: liked? 'red' : 'black'
+    }
     
-    return (<span onClick={addLike} className={styles.likesCounter} id={id}>{likes}</span>)
+    return (<span style={style} onClick={addLike} className={styles.likesCounter} id={id}>{likes}</span>)
 }
 
 export default LikesCounter;
