b58492ce146d597a65a324c2137192c9572bddb5
MariaDikhtiievska <91692751+MariaDikhtiievska@users.noreply.github.com>
2022-02-14 11:39:33 +0200

    Delete React4 directory

--- React4/les/src/elements/CommentsCounter.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from '../components/styles.module.scss'
-
-
-const CommentsCounter = ({comments}) =>{
-return (<span className={styles.commentsCounter}>{comments}</span>)}
-
-export default CommentsCounter;
--- React4/les/src/elements/Tag.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from '../components/styles.module.scss'
-
-
-const Tag = ({tags}) =>{
-return (<span className={styles.tag}>{tags}</span>)}
-export default Tag;
